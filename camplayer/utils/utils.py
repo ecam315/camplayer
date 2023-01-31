9abfdf78d6436412405ce5b9492eeb3185a31acb
ecam315 <ecameron315@gmail.com>
2023-01-31 10:48:06 -0500

    Update utils.py

--- camplayer/utils/utils.py
+++ camplayer/utils/utils.py
@@ -29,6 +29,7 @@ pi_revisions = {
     "d03114" : {"model": "4B 8GB",      "supported": True, "dual_hdmi": True,  "hevc": True},
     "b03114" : {"model": "4B 2GB",      "supported": True, "dual_hdmi": True,  "hevc": True},
     "c03114" : {"model": "4B 4GB",      "supported": True, "dual_hdmi": True,  "hevc": True},
+    "c03115" : {"model": "4B 4GB",      "supported": True, "dual_hdmi": True,  "hevc": True},
     "c03130" : {"model": "PI 400 4GB",  "supported": True, "dual_hdmi": True,  "hevc": True},
     "9020e0" : {"model": "3A+",         "supported": True, "dual_hdmi": False, "hevc": False},
 }
